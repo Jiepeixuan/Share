55dabdfe147c33f1e7bc973783f08902c0243994
解佩旋 <jiepeixuan@jiepeixuandeMacBook-Pro.local>
2017-05-31 23:26:19 +0800

    reactDemo

---
+++ React/reactDemo/src/js/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ComponentFooter extends React.Component{
+    render(){
+        return(
+            <footer>
+                <h1>底部</h1>
+            </footer>
+        )
+    }
+}
--- React/readme.md
+++ React/readme.md
@@ -4,3 +4,5 @@ sudo npm install babel-preset-es2015
 npm install babel-core babel-loader babel-preset-es2015 babel-preset-react --save-dev
 
 webpack --watch   自动编译
+
+ webpack-dev-server --contentbase src --inline --hot
