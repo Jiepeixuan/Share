26b6f5dde1fd08b3261b8c0e25ef0cecc75a053c
解佩旋 <459062059@qq.com>
2017-07-07 00:17:33 +0800

    lesson1

--- ES6/project/app/js/class/lesson1.js
+++ ES6/project/app/js/class/lesson1.js
@@ -1,6 +1,14 @@
 function test(){
     var a=2;
     console.log(a);
+    const PI=3.1415926;
+    console.log(PI);
+
+    const k={
+        a:1
+    }
+    k.a=2;
+    console.log(PI,k);
 }
 
 test();
